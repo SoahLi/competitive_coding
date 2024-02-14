c09c8a6a0dad0928e1b6b981f59324455bdd23fd
SoahLi <ows8turnbull@gmail.com>
2024-02-14 13:59:55 -0500

    Palindrome Partitioning

--- lc_m/palindrome_partitioning.cc
+++ lc_m/palindrome_partitioning.cc
@@ -45,51 +45,147 @@ I literally have the right answer I just can't figure out how to code it
 //     return result;
 // }
 
-class Solution {
-public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> path;
-        helper(0, s, path, res);
-        return res;
+// class Solution {
+// public:
+//     vector<vector<string>> partition(string s) {
+//         vector<vector<string>> res;
+//         vector<string> path;
+//         helper(0, s, path, res);
+//         return res;
+//     }
+
+//     void helper(int index, string s, vector<string> path, vector<vector<string>>& result) {
+//         if(index >= s.size()) {
+//             result.push_back(path);
+//             return;
+//         }
+
+//         for(int i=index; i<s.size(); i++) {
+//             if(isPalindrome(s, index, i)) {
+//                 path.push_back(s.substr(index, i-index+1));
+//                 helper(i+1, s, path, result);
+//                 path.pop_back();
+//             }
+//         }
+//     }
+
+
+//     bool isPalindrome(string s, int start, int end){
+//         while(start <= end){
+//             if(s[start++] != s[end--]) return false;
+//         }
+//         return true;
+//     }
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bool isPalindrome(const std::string& str) {
+    std::string reversed = str;
+    std::reverse(reversed.begin(), reversed.end());
+    return str == reversed;
+}
+
+void helper(int i, string& s, vector<string> current, vector<vector<string> >& result) {
+    if(i >= s.size()) {
+        result.push_back(current);
+        return;
     }
 
-    void helper(int index, string s, vector<string> path, vector<vector<string>>& result) {
-        if(index >= s.size()) {
-            result.push_back(path);
-            return;
-        }
+    string str;
+    for(int j=1; j<=(s.size()-i); j++) {
 
-        for(int i=index; i<s.size(); i++) {
-            if(isPalindrome(s, index, i)) {
-                path.push_back(s.substr(index, i-index+1));
-                helper(i+1, s, path, result);
-                path.pop_back();
-            }
+        str = s.substr(i, j);
+        if(isPalindrome(str)) {
+
+            current.push_back(str);
+            helper(i+j, s, current, result);
+            current.pop_back();            
+
         }
     }
 
+}
 
-    bool isPalindrome(string s, int start, int end){
-        while(start <= end){
-            if(s[start++] != s[end--]) return false;
+vector<vector<string> > partition(string s) {
+    vector<vector<string> > result;
+    vector<string> current;
+    string str;
+    for(int i=0; i<s.size(); i++) {
+        str = s.substr(0, i+1);
+        if(isPalindrome(str)) {
+            current.clear();
+            current.push_back(str);
+            helper(i+1, s, current, result);
         }
-        return true;
     }
 
-};
+    return result;
+}
+
 
 int main() {
-    Solution solution;
-    string str = "ababa";
-    vector<vector<string>> ans = solution.partition(str);
-    cout << "hello" << '\n';
+    string str = "abbab";
+    vector<vector<string> > ans = partition(str);
     for(vector<string> a : ans) {
         for(string part : a) {
             cout << part << " ";
         }
         cout << endl;
     }
-    
 
 }
